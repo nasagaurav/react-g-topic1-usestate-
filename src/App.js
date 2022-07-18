69c04922a05b104a04c0c8e28a5c7ad379043d91
nasagaurav <nasa.gauravnasa@gmail.com>
2022-07-18 17:35:13 +0530

    1 file modified

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-import React from "react";
-import "./style.css";
+import React from 'react';
+import './style.css';
 
 export default function App() {
   return (
     <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+      <h1>we are practising usestate in reactjs-project 1</h1>
+      <h1>counter ? </h1>
+      <button>+</button>
+      <button>-</button>
+      <button>0</button>
     </div>
   );
 }
