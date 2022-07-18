7dae079a5f5c5f27d5260d73a68ffec84b6a8c9f
nasagaurav <nasa.gauravnasa@gmail.com>
2022-07-18 17:53:45 +0530

    functionality defined

--- src/App.js
+++ src/App.js
@@ -2,14 +2,22 @@ import React from 'react';
 import './style.css';
 
 export default function App() {
-  const p1 = () => {};
-  const p2 = () => {};
-  const p3 = () => {};
+
+    const [x, setx] = React.useState(0);
+
+      console.log( React.useState(0))
+
+
+  const p1 = () =>
+   {setx (x+1)
+   };
+  const p2 = () => {setx (x-1)};
+  const p3 = () => { setx (0);};
 
   return (
     <div>
-      <h1>we are practising usestate in reactjs-project 1</h1>
       <h1>counter ? </h1>
+      <h1>counter {x} </h1>
 
       <button onClick={p1}>+</button>
       <button onClick={p2}>-</button>
