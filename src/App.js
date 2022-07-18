a0d5c29c2776c89dd4c55b4702de68212541faad
nasagaurav <nasa.gauravnasa@gmail.com>
2022-07-18 18:14:18 +0530

    added functionality

--- src/App.js
+++ src/App.js
@@ -2,15 +2,23 @@ import React from 'react';
 import './style.css';
 
 export default function App() {
+    const a = [a,seta] = React.useState([]);
+
+    const p1 =()=>{ 
+    
+      seta=( [...a,' '] );
+    }
+
 return(
  <div>
-   <button>add more questions</button>
-<h1>total questions</h1>
+   <button onClick={p1}>add more questions</button>
+
+<h1>total questions {a.length}</h1>
+
 <ol>
-  <li>question </li>
+{a.map(item=>  <li>question </li>)}
+
 </ol>
-
-
 
    </div>
 );
