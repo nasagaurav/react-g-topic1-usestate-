49ddba0ec16bf7bb40747b5374b6a6d2609ff8f2
nasagaurav <nasa.gauravnasa@gmail.com>
2022-07-18 17:40:14 +0530

    onclick

--- src/App.js
+++ src/App.js
@@ -2,13 +2,18 @@ import React from 'react';
 import './style.css';
 
 export default function App() {
+  const p1 = () => {};
+  const p2 = () => {};
+  const p3 = () => {};
+
   return (
     <div>
       <h1>we are practising usestate in reactjs-project 1</h1>
       <h1>counter ? </h1>
-      <button>+</button>
-      <button>-</button>
-      <button>0</button>
+
+      <button onClick={p1}>+</button>
+      <button onClick={p2}>-</button>
+      <button onclick={p3}>0</button>
     </div>
   );
 }
